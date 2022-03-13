95f0ec88c02a62a6def4d209e86d8f812437ebf6
Gaaaamma <ytching552022@gmail.com>
2022-03-14 00:45:21 +0800

    map output structure

--- hw1.cpp
+++ hw1.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 #include <regex>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
 #include <string>
+#include <map>
+#define bufferSize 256
 
 using namespace std;
 
 string typeCheck(dirent* dirp);
+string extractInput(char buffer[bufferSize],int readCount);
+void traverseMap(map<string,string> mMap); 
 
 int main(int argc, char* argv[]){
 	string defPath = "/proc/";
 
 	DIR* dp;
 	struct dirent* dirp;
-	int counter =0;
 	regex  procReg("^[0-9]+$");	
+	map<string,string> outputMap;
 
 	cout << "COMMAND\t\tPID\t\tUSER\t\tFD\t\tTYPE\t\tNODE\t\tNAME\n";
 
 	if((dp = opendir(defPath.c_str())) != NULL){
 		// Got something
 		while((dirp = readdir(dp)) != NULL){
-			// only DIR && number can to go ahead
+			// only DIR && number can go ahead
 			if(typeCheck(dirp) == "DIR" && regex_search(dirp->d_name,procReg)){
 				// Handle command
 				string procPath = defPath + dirp->d_name ;
+				string procPathComm = procPath + "/comm";
+				char buffer[bufferSize]={};
+				int procFd =0;
 
+				if((procFd = open(procPathComm.c_str(),O_RDONLY)) != -1){
+					int length = read(procFd,buffer,sizeof(buffer));
+					outputMap["COMMAND"] = extractInput(buffer,length);
+					close(procFd);
+
+				}else{
+					// error occur
+					cout << "open("+procPath+",O_RDONLY) FAIL\n";
+				}
 				// Handle PID
-				cout << temp << "\t\t" << dirp->d_name <<endl;
+				outputMap["PID"] = dirp->d_name ;
 
 				// Handle USER
 
@@ -39,8 +58,7 @@ int main(int argc, char* argv[]){
 				// Handle NODE
 
 				// Handle NAME
-
-				counter ++;
+				traverseMap(outputMap);				
 			}
 			/*
 			cout << "inode number: " << dirp->d_ino <<endl;
@@ -67,7 +85,6 @@ int main(int argc, char* argv[]){
 				cout << argv[1] <<" is not a directory\n";
 		}
 	}
-	cout << counter <<endl;
 	return 0;
 }
 
@@ -95,3 +112,22 @@ string typeCheck(dirent* dirp){
 	}
 	return result;
 }
+
+string extractInput(char buffer[bufferSize],int readCount){
+	string result ="";
+    for(int i=0;i<readCount;i++){
+    	if(buffer[i]=='\n' || buffer[i]=='\r' || buffer[i]=='\0'){
+           break ;
+    	}
+       	result += buffer[i] ;
+   	}
+   	return result; 
+}
+void traverseMap(map<string,string> mMap){
+	map<string,string>::iterator iter = mMap.begin();
+	while(iter != mMap.end()){
+		cout << iter->second <<"\t\t";
+		iter ++;
+	}
+	cout << endl;
+} 
